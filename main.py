ba9dad2c7866bca3b0bad35da76e3071991c7522
kohmoli <omori.fuki@gmail.com>
2023-05-24 23:37:02 +0900

    DB,VueJS削除

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import sqlite3
+from wsgiref.simple_server import make_server, WSGIServer
+from urllib.parse import parse_qs
+from socketserver import ThreadingMixIn
+import time
+import json
 import subprocess
+
+
+
+messages = []
+token_length = {}
 
 
 
@@ -11,43 +17,85 @@ def save_log(message):
 
 
 
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+class App:
+    def __init__(self, start_response):
+        self.start_response = start_response
+    
+    
 
-cursor = sqlite3.connect("app.db", isolation_level=None, check_same_thread=False).cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS message(id, author, body)""")
-
-
-
-class Message(BaseModel):
-    author: str
-    body: str
-
+    def read_item(self, token):
+        if token in token_length:
+            for _ in range(3600):
+                if token_length[token] < len(messages):
+                    break
+                time.sleep(1)
+        
+        token_length[token] = len(messages)
+        self.start_response('200 OK', [('Content-type', 'application/json')])
+        return [json.dumps(messages).encode('utf-8')]
 
 
-@app.get("/")
-def read_root():
-    cursor.execute("""SELECT * FROM message""")
-    return {"Hello": cursor.fetchall()}
 
-@app.get("/messages")
-def read_item():
-    cursor.execute("""SELECT * FROM message""")
-    return cursor.fetchall()
+    def insert_message(self, message):
+        messages.append(message)
+        self.start_response('200 OK', [('Content-type', 'application/json')])
+        print(len(messages))
+        return [json.dumps({"status": "done"}).encode('utf-8')]
 
-@app.get("/messages/{message_id}")
-def read_item(message_id: int):
-    cursor.execute(f"""SELECT * FROM message WHERE id={message_id}""")
-    return cursor.fetchone()
 
-@app.post("/post")
-def insert_message(message: Message):
-    cursor.execute("""INSERT INTO message VALUES(?, ?, ?)""", (0, message.author, message.body))
-    save_log(message.body)
-    return {"status": "done"}
+
+    def index_html(self):
+        self.start_response('200 OK', [('Content-type', 'text/html')])
+        with open('page/index.html', 'r') as f:
+            return [f.read().encode('utf-8')]
+
+
+
+    def main_js(self):
+        self.start_response('200 OK', [('Content-type', 'text/javascript')])
+        with open('page/main.js', 'r') as f:
+            return [f.read().encode('utf-8')]
+
+
+
+    def style_css(self):
+        self.start_response('200 OK', [('Content-type', 'text/css')])
+        with open('page/style.css', 'r') as f:
+            return [f.read().encode('utf-8')]
+
+
+
+    def not_found(self):
+        self.start_response('404 Not Found', [])
+        return b''
+
+
+
+class ThreadingWSGIServer(ThreadingMixIn, WSGIServer):
+    pass
+
+
+
+def app(environ, start_response):
+    response = App(start_response)
+    query = parse_qs(environ['QUERY_STRING'])
+
+    match environ['PATH_INFO']:
+        case '/':
+            return response.index_html()
+        case '/main.js':
+            return response.main_js()
+        case '/style.css':
+            return response.style_css()
+        case '/messages':
+            return response.read_item(query['token'][0])
+        case '/post':
+            return response.insert_message(json.loads(environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', 0)))))
+        case _:
+            return response.not_found()
+
+
+
+with make_server('', 8080, app, ThreadingWSGIServer) as httpd:
+    print('Serving on port 8080')
+    httpd.serve_forever()
--- page/main.js
+++ page/main.js
@@ -1,45 +1,39 @@
-const messageList = new Vue({
-    el: '#content',
-    data: {
-        messages: []
-    },
-    mounted: function() {
-        fetch("http://localhost:8000/messages")
-        .then(res => res.json())
-        .then(data => messageList.messages = data);
-    },
-    methods: {
-        updateMessage: function(){
-            fetch("http://localhost:8000/messages")
-            .then(res => res.json())
-            .then(data => messageList.messages = data);
-        }
-    }
-})
 
-const submitForm = new Vue({
-    el: '#submitForm',
-    data: {
-        ip_address: '',
-        body: ''
-    },
-    async mounted() {
-        fetch("https://ipinfo.io/ip")
-        .then(res => res.text())
-        .then(data => submitForm.ip_address = data);
-    },
-    methods: {
-        submit: function(){
-            const param = {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({
-                    author: this.ip_address,
-                    body: this.body
-                })
-            };
-            fetch("http://localhost:8000/post/", param)
-            .then(messageList.updateMessage());
-        }
+const token = '' + Math.random();
+var ip_address;
+
+fetch("https://ipinfo.io/ip")
+.then(res => res.text())
+.then(data => ip_address = data);
+
+const fetchMessage = () => {
+    fetch("http://localhost:8080/messages?token=" + token)
+    .then(res => res.json())
+    .then(data => {updateMessage(data); fetchMessage()});
+}
+
+const updateMessage = (messages) => {
+    const messageElement = document.getElementsByClassName("message")[0];
+    document.getElementById("content").innerHTML = "";
+    for(const message of messages){
+        const new_message = messageElement.cloneNode(true);
+        new_message.getElementsByClassName("author")[0].innerHTML = message["author"];
+        new_message.getElementsByClassName("body")[0].innerHTML = message["body"];
+        document.getElementById("content").appendChild(new_message);
     }
-})
+}
+fetchMessage();
+
+
+const submit = () => {
+    const param = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            author: ip_address,
+            body: document.getElementById("submitText").value
+        })
+    };
+    fetch("http://localhost:8080/post", param)
+    .then(document.getElementById("submitText").value = "");
+}
